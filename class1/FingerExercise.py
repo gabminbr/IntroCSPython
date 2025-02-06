f5a41b39eaa2632302076a5e61e54bbe4fc65126
Bowie <gabmineiro123@hotmail.com>
2025-02-06 17:08:24 -0400

    class 1 done!

---
+++ class1/FingerExercise.py
@@ -0,0 +1,13 @@
+# we can use type(object) to know what is the data type of the object
+# round(number) to round it to the next integer value
+
+# Finger Exercise Lecture 1
+
+# initializing 3 variables, and allocatin the sum between 'a' and 'b'
+# and allocating the result in a total variable and print the total
+a = 10
+b = 20
+c = 30
+total = (a + b) * c
+
+print(total)
